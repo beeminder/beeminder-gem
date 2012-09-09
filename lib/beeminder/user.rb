be7aad753b82974fe343026299cd8698857f2c52
muflax <mail@muflax.com>
2012-09-09 08:19:44 +0200

    more useful output on error

--- lib/beeminder/user.rb
+++ lib/beeminder/user.rb
@@ -128,7 +128,7 @@ module Beeminder
         
         res = http.request(req)
         if not res.is_a? Net::HTTPSuccess
-          raise "request failed: #{res.body}"
+          raise "request #{cmd} / #{data} failed: #{req.code} / #{res.body}"
         end
 
         json = res.body
